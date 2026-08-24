9312cf74bf54a9b1b838a6272ea5fb3088c61e4d
khushboo-bhola <khushboobhola12@gmail.com>
2026-08-25 01:52:52 +0530

    week7

---
+++ week_assignment/week7/Balanced_Parenthesis.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Balanced_Parenthesis {
+
+    public static boolean isBalanced(String expression) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < expression.length(); i++) {
+            char ch = expression.charAt(i);
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            }
+            else if (ch == ')' || ch == '}' || ch == ']') {
+
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if (ch == ')' && top != '(') {
+                    return false;
+                }
+
+                if (ch == '}' && top != '{') {
+                    return false;
+                }
+
+                if (ch == ']' && top != '[') {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String expression = sc.nextLine();
+
+        System.out.println(isBalanced(expression));
+    }
+}
---
+++ week_assignment/week7/Postfix_Expressions.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Postfix_Expressions {
+    public static int evaluate(String expression) {
+        Stack<Integer> stack = new Stack<>();
+        String[] tokens = expression.split(" ");
+        for (String token : tokens) {
+            if (token.equals("+") || token.equals("-") ||
+                token.equals("*") || token.equals("/")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                if (token.equals("+")) {
+                    stack.push(a + b);
+                }
+                else if (token.equals("-")) {
+                    stack.push(a - b);
+                }
+                else if (token.equals("*")) {
+                    stack.push(a * b);
+                }
+                else if (token.equals("/")) {
+                    stack.push(a / b);
+                }
+            }
+            else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+        return stack.pop();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String expression = sc.nextLine();
+        System.out.println(evaluate(expression));
+    }
+}
